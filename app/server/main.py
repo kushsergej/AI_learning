98a0b9f462de1a55a30a97f748ef60e4723f0e75
Siarhei Kushniaruk <siarhei_kushniaruk@epam.com>
2025-12-08 23:11:01 +0100

    MCP server started

---
+++ app/server/main.py
@@ -0,0 +1,20 @@
+from mcp.server.fastmcp import FastMCP
+
+
+# FastMCP
+mcp = FastMCP('Revolut')
+
+
+REVOLUT_API_EUR_PLN = 'https://www.revolut.com/currency-converter/convert-eur-to-pln-exchange-rate?amount=38757.37'
+REVOLUT_API_PLN_EUR = 'https://www.revolut.com/currency-converter/convert-pln-to-eur-exchange-rate?amount=164519.28'
+
+
+
+
+# Tools
+
+# Resources
+
+# Prompts
+
+
--- start.sh
+++ start.sh
@@ -11,19 +11,19 @@ uv add -r requirements.txt
 # decouple LLM and code (save LLM weights locally)
 uv run app/download_model.py
 
-docker build \
-    -t kushsergej-llm:v1 \
-    -f app/Dockerfile \
-    app/
-docker image ls
+# docker build \
+#     -t kushsergej-llm:v1 \
+#     -f app/Dockerfile \
+#     app/
+# docker image ls
 
-docker rm -f llm_backend 2>/dev/null || true
-MSYS_NO_PATHCONV=1 docker run -d \
-    -v $(pwd)/app/model_snapshot:/app/model_snapshot \
-    -p 8000:8000 \
-    --name llm_backend \
-    kushsergej-llm:v1
-docker container ls
+# docker rm -f llm_backend 2>/dev/null || true
+# MSYS_NO_PATHCONV=1 docker run -d \
+#     -v $(pwd)/app/model_snapshot:/app/model_snapshot \
+#     -p 8000:8000 \
+#     --name llm_backend \
+#     kushsergej-llm:v1
+# docker container ls
 
 
 
@@ -32,6 +32,3 @@ docker container ls
 # ------------------------------------- #
 # curl --request GET http://localhost:8000/docs
 # curl --request POST --header 'Content-Type: application/json' --data '{"message": "Who is the Rome Pope now?"}' http://localhost:8000/generate
-
-# MCP https://www.revolut.com/currency-converter/convert-eur-to-pln-exchange-rate?amount=38757.37
-# MCP https://www.revolut.com/currency-converter/convert-pln-to-eur-exchange-rate?amount=164540
