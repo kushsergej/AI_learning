3cc8656edbe3d976bed200adea6b9b4861c87b3d
Siarhei Kushniaruk <siarhei_kushniaruk@epam.com>
2025-12-11 00:00:32 +0100

    MCP server continue

--- app/server/main.py
+++ app/server/main.py
@@ -1,4 +1,7 @@
+from typing import Any
+import httpx
 from mcp.server.fastmcp import FastMCP
+import asyncio
 
 
 # FastMCP
@@ -10,11 +13,55 @@ REVOLUT_API_PLN_EUR = 'https://www.revolut.com/currency-converter/convert-pln-to
 
 
 
+# Tools (specific operation with typed inputs and outputs)
+async def make_get_request(url: str) -> dict[str, Any]:
+    '''
+    Make a GET request to API
+    Args:
+        url: The URL to make the request to
+    Returns:
+        The data from the request
+    '''
+    headers = {
+        'Accept': 'application/json'
+    }
+    async with httpx.AsyncClient(follow_redirects=True) as client:
+        try:
+            response = await client.get(url, headers=headers, timeout=10.0)
+            response.raise_for_status()
+            return response.json()
+        except Exception as e:
+            return {'error': str(e)}
 
-# Tools
+
+@mcp.tool()
+async def get_revolut_exchange_rate(amount: float, currency: str) -> dict[str, Any]:
+    '''
+    Get the exchange rate for a given amount and currency
+    Args:
+        amount: The amount to convert
+        currency: The currency to convert to
+    Returns:
+        The exchange rate
+    '''
+    url = REVOLUT_API_EUR_PLN if currency == 'EUR' else REVOLUT_API_PLN_EUR
+    # data = await make_get_request(url)
+    data = await make_get_request(REVOLUT_API_EUR_PLN)
+    return data
+
+
+
+
+
 
 # Resources
 
 # Prompts
 
 
+async def main():
+    # Initialize and run the server
+    await mcp.run(transport='stdio')
+
+if __name__ == "__main__":
+    asyncio.run(main())
